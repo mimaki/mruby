b095e967c62e4cd88e584d28ec6231f2e78a7d13
Hiroshi Mimaki <hiroshi_mimaki@fukuoka-csk.co.jp>
2012-10-25 16:17:35 +0900

    fixed compile error on msys.

--- ext/armadillo/acce_mma7455l.c
+++ ext/armadillo/acce_mma7455l.c
@@ -11,20 +11,14 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <ctype.h>
+#if !defined(_WIN32) && !defined(_WIN64)
 #include <sys/ioctl.h>
 #include <linux/i2c-dev.h>
+#endif /* _WINxx */
 
 #include "mma7455l.h"
 
-//#define MMA7455LTEST
-//#define MAIN_C
-
-//#define BASENAME(p)   ((strrchr((p), '/') ? : ((p) - 1)) + 1)
-
-
 #define MMA7455L_RETRY_MAX 3 /* read()とwrite()のリトライ回数 */
-//#define DEVICE_NAME "/dev/i2c-1"
-//#define I2C_ADDR        0x1D    /* MMA7455LのI2Cアドレス */
 
 #define ADJUSTX (7)
 #define ADJUSTY (21)
@@ -34,34 +28,26 @@
 
 #define LIMIT(v,lim)  (int8_t)(((v) < -(lim)) ? -(lim) : ((v) > (lim)) ? (lim) : (v))
 
+#if !defined(_WIN32) && !defined(_WIN64)
+
 #ifdef MMA7455LTEST
 
-//struct mma7455l {
-//        int fd;
-//};
 void debug_print(char*str){
 //  printf( "%s\n" , str);
 }
 
 int main()
 {
-//  struct mma7455l *mmafd;
   int fd;
   int8_t x,y,z;
   int ret;
-//  int count = 10;
 
-//  mmafd = mma7455l_open(DEVICE_NAME, I2C_ADDR);
   fd = mma7455l_open(DEVICE_NAME, I2C_ADDR);
-//  if (mmafd == NULL){
   if (fd < 0) {
     debug_print("mma7455l can't open");
     return -1;
   }
-  //  while (count--){
   while (1){
-//    ret = mma7455l_read(mmafd, &x , &y , &z);
-//    if (ret == -1){
     if (mma7455l_read(fd, &x , &y , &z) < 0) {
       debug_print("mma7455l can't read");
       return -1;
@@ -69,8 +55,6 @@ int main()
     printf("x=%d,y=%d,z=%d\n", x,y,z);
   }
 
-//  ret = mma7455l_close(mmafd);
-//  if (ret == -1){
   if (mma7455l_close(fd)) {
     debug_print("mma7455l can't close");
     return -1;
@@ -122,6 +106,8 @@ static ssize_t read_uninterruptible(int fd, void *buf, size_t count, int retry)
   return -1;
 }
 
+#endif /* _WINxx */
+
 /**
  * 指定されたデバイスファイルをオープンする
  *
@@ -134,11 +120,8 @@ static ssize_t read_uninterruptible(int fd, void *buf, size_t count, int retry)
 //struct mma7455l *mma7455l_open(const char *dev_path, const int addr)
 int mma7455l_open(const char *dev_path, const int addr)
 {
-//  struct mma7455l *mmafd;
+#if !defined(_WIN32) && !defined(_WIN64)
   int fd;
-//  int error;
-//  int ret;
-//  uint8_t buf[1];
   uint8_t buf[2];
   
   if (dev_path == NULL) {
@@ -146,26 +129,11 @@ int mma7455l_open(const char *dev_path, const int addr)
     return -EINVAL;
   }
 
-//  mmafd = calloc(1, sizeof(struct mma7455l));
-//  if (mmafd == NULL)
-//    return NULL;
-//  mmafd->fd = open(dev_path, O_RDWR);
-//  printf("mmafd->fd=%d\n",mmafd->fd);  
-//  if (mmafd->fd < 0) {
-//    error = errno;
-//    goto err1;
-//  }
   fd = open(dev_path, O_RDWR);
-//  printf("fd=%d\n", fd);
   if (fd < 0) {
     return fd;
   }
 
-//  ret = ioctl(mmafd->fd, I2C_SLAVE, addr);
-//  if (ret < 0) {
-//    error = errno;
-//    goto err2;
-//  }
   if (ioctl(fd, I2C_SLAVE, addr) < 0) {
     close(fd);
     return -1;
@@ -174,10 +142,6 @@ int mma7455l_open(const char *dev_path, const int addr)
   //アドレス0x16に0x05を書き込む
   buf[0] = 0x16;
   buf[1] = 0x05;
-//  ret = write_uninterruptible(mmafd->fd, buf ,2, MMA7455L_RETRY_MAX);
-//  if (ret < 0)
-//    return NULL;
-//  return mmafd;
   if (write_uninterruptible(fd, buf, 2, MMA7455L_RETRY_MAX) < 0) {
     close(fd);
     return -1;
@@ -185,12 +149,9 @@ int mma7455l_open(const char *dev_path, const int addr)
 
   return fd;
 
-// err2:
-//  close(mmafd->fd);
-// err1:
-//  free(mmafd);
-//  errno = error;
-//  return NULL;
+#else
+  return 0;
+#endif /* _WINxx */
 }
 
 
@@ -204,58 +165,27 @@ int mma7455l_open(const char *dev_path, const int addr)
  * @return 成功すると0を返し、x,y,zに値を格納する。
  *         失敗すると-1を返す。
  */
-//int mma7455l_read(struct mma7455l *mmafd, int8_t *x, int8_t *y , int8_t *z)
 int mma7455l_read(int fd, int8_t *x, int8_t *y , int8_t *z)
 {
-//  uint8_t buf[4];
+#if !defined(_WIN32) && !defined(_WIN64)
   uint8_t cmd[1];
   int8_t buf[MMA7455L_CH_MAX];
-//  int ret;
   int ix, iy, iz;
 
-//  if (mmafd == NULL || x == NULL || y == NULL || z == NULL) {
   if (fd < 0 || x == NULL || y == NULL || z == NULL) {
     errno = EINVAL;
     return -1;
   }
 
-//  buf[0] = 0x06;
-//  ret = write_uninterruptible(mmafd->fd, buf, 1, MMA7455L_RETRY_MAX);
-//  if (ret < 0){
   cmd[0] = 0x06;
   if (write_uninterruptible(fd, cmd, sizeof(cmd), MMA7455L_RETRY_MAX) < 0) {
     return -1;
   }
 
   /* x,y,zの3バイト読み込む */
-//  ret = read_uninterruptible(mmafd->fd, buf, 3, MMA7455L_RETRY_MAX);
-//  if (ret < 0){
   if (read_uninterruptible(fd, buf, sizeof(buf), MMA7455L_RETRY_MAX) < 0) {
     return -1;
   }
-
-//  *x = buf[0] + ADJUSTX;
-//  *y = buf[1] + ADJUSTY;
-//  *z = buf[2] + ADJUSTZ;
-
-//  if (*x > 63){
-//    *x = 63;
-//  }
-//  else if (*x < -63){
-//    *x = -63;
-//  }
-//  if (*y > 63){
-//    *y = 63;
-//  }
-//  else if (*y < -63){
-//    *y = -63;
-//  }
-//  if (*z > 63){
-//    *z = 63;
-//  }
-//  else if (*z < -63){
-//    *z = -63;
-//  }
 
   ix = DEGREE(buf[0] + ADJUSTX);
   iy = DEGREE(buf[1] + ADJUSTY);
@@ -264,6 +194,7 @@ int mma7455l_read(int fd, int8_t *x, int8_t *y , int8_t *z)
   *x = LIMIT(ix, 90);
   *y = LIMIT(iy, 90);
   *z = LIMIT(iz, 90);
+#endif /* _WINxx */
 
   return 0;
 }
@@ -279,15 +210,9 @@ int mma7455l_read(int fd, int8_t *x, int8_t *y , int8_t *z)
 //int mma7455l_close(struct mma7455l *mmafd)
 int mma7455l_close(int fd)
 {
-//  int fd;
-//
-//  if (mmafd == NULL) {
-//    errno = EINVAL;
-//    return -1;
-//  }
-//
-//  fd = mmafd->fd;
-//  free(mmafd);
-//
+#if !defined(_WIN32) && !defined(_WIN64)
   return close(fd);
+#else
+  return 0;
+#endif /* _WINxx */
 }
