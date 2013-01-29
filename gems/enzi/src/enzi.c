8a8fcfadc024af880db1d1183d90010d774a5a2e
Hiroshi Mimaki <hiroshi_mimaki@fukuoka-csk.co.jp>
2013-01-29 20:13:19 +0900

    ENZI_JS support.

---
+++ gems/enzi/src/enzi.c
@@ -0,0 +1,25 @@
+/*
+** enzi.c - enzi Library
+**
+** Copyright(c) FUKUOKA CSK CORPORATION
+** Copyright(c) Manycolors Inc
+*/
+
+#include "mruby.h"
+#include "mruby/variable.h"
+
+void mrb_init_digitalio(mrb_state *mrb);
+void mrb_init_analogio(mrb_state *mrb);
+
+void
+mrb_enzi_gem_init(mrb_state *mrb)
+{
+  mrb_init_digitalio(mrb);
+  mrb_init_analogio(mrb);
+}
+
+void
+mrb_enzi_gem_final(mrb_state *mrb)
+{
+}
+
